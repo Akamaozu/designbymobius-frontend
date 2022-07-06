1cea5c77315aaaebd23d75ed3064fb7db6e4d8f6
Uzo Olisemeka <uzo@designbymobius.ca>
2022-07-06 15:27:20 -0400

    convert blank react app to personal site base

--- src/App.js
+++ src/App.js
@@ -1,23 +1,27 @@
-import logo from './logo.svg';
 import './App.css';
 
 function App() {
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="App-header header">
+        <div className="logo">designbymobius</div>
+      </div>
+      <div className="App-content">
+        <h3>Software Engineer</h3>
+        <h5>
+          <span className="emphasis">JavaScript</span>
+          {" "} + <span className="emphasis">Node.js</span>
+          {" "} + <span className="emphasis">React</span>
+         </h5>
+        <div className="categories">
+          <div className="category">
+            <div className="category-title">Resume</div>
+          </div>
+          <div className="category">
+            <div className="category-title">Projects</div>
+          </div>
+        </div>
+      </div>
     </div>
   );
 }
