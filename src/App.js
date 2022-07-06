91f7a47414d55196ed2e6d4d98137a9c5fa26f87
Uzo Olisemeka <uzo@designbymobius.ca>
2022-07-06 17:00:35 -0400

    conditionally load react component for facebook chat

--- src/App.js
+++ src/App.js
@@ -1,10 +1,14 @@
+import Logo from './components/Logo';
+import FacebookChat from './components/FacebookChat';
 import './App.css';
+
+const FACEBOOK_PAGE_ID = process.env.REACT_APP_FACEBOOK_PAGE_ID;
 
 function App() {
   return (
     <div className="App">
       <div className="App-header header">
-        <div className="logo">designbymobius</div>
+        <Logo />
       </div>
       <div className="App-content">
         <h3>Software Engineer</h3>
@@ -25,6 +29,11 @@ function App() {
           </div>
         </div>
       </div>
+      {
+        FACEBOOK_PAGE_ID
+          ? <FacebookChat pageId={ FACEBOOK_PAGE_ID } />
+          : null
+      }
     </div>
   );
 }
