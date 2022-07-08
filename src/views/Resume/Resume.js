472cba7d67ca5fefaa6f36865cfe43c610629ef8
Uzo Olisemeka <uzo@designbymobius.ca>
2022-07-08 07:48:59 -0400

    render work experience in resume view

--- src/views/Resume/Resume.js
+++ src/views/Resume/Resume.js
@@ -1,13 +1,47 @@
 import ViewTitle from '../../components/ViewTitle'
 import ViewSubtitle from '../../components/ViewSubtitle'
+import data from '../../data'
+import './style.css'
+
+const WorkExperience = props => {
+  const experience = props.experience ?? {}
+  const duration = experience.start === experience.end ? experience.start : `${experience.start} - ${experience.end}`
+
+  return (
+    <div className="workExperience">
+      <div className="workExperience-title">{ experience.title }</div>
+      <div className="workExperience-when">{ duration }</div>
+    </div>
+  )
+}
 
 const Resume = () => {
+  const workExperiences = data?.workExperiences ?? []
+  const sortedWorkExperiences = [...workExperiences].sort((a,b) => {
+    // sort newer start date higher
+    if (a.start > b.start) return -1
+    if (a.start < b.start) return 1
+
+    // sort newer end date higher
+    if (a.end > b.end) return -1
+    if (a.end < b.end) return 1
+
+    return 0
+  })
+
   return (
     <>
       <ViewTitle>Resume</ViewTitle>
       <ViewSubtitle>
         Curated experience from <span className="emphasis">10+ years writing code</span>
       </ViewSubtitle>
+      <div className="workExperiences">
+        {
+          sortedWorkExperiences.map(experience => {
+            return <WorkExperience experience={experience} />
+          })
+        }
+      </div>
     </>
   )
 }
