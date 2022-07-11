fe840d04c2ca6984e7bce5b992048e529baada6e
Uzo Olisemeka <uzo@designbymobius.ca>
2022-07-11 15:28:33 -0400

    fix: ensure server properly handles non-root request paths

--- server.js
+++ server.js
@@ -1,11 +1,18 @@
 require('dotenv').config()
 
 const express = require('express')
-const server = express()
+const fs = require('fs')
 
+const server = express()
 const PORT = process.env.PORT ?? 3000
+const html = fs.readFileSync('./build/index.html', { encoding: 'utf8' })
 
 server.use(express.static('build'))
+
+server.get('/*', (req, res) => {
+  res.setHeader('content-type', 'text/html')
+  res.send(html)
+})
 
 server.listen(PORT, ()=> {
   console.log(`action=server-listen port=${ PORT }`)
