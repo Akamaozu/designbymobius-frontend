cf95e68c14a4b477803b0509949c239ebbb2a396
Uzo Olisemeka <uzo@designbymobius.ca>
2022-07-19 17:40:28 -0400

    added logging to server startup

--- server.js
+++ server.js
@@ -1,19 +1,26 @@
 require('dotenv').config()
+console.log(`action=load-env-vars count=${(Object.keys(process.env)).length}`)
 
 const express = require('express')
 const fs = require('fs')
 
-const server = express()
-const PORT = process.env.PORT ?? 3000
-const html = fs.readFileSync('./build/index.html', { encoding: 'utf8' })
+const start = async () => {
+  const server = express()
+  const PORT = process.env.PORT ?? 3000
+  const html = fs.readFileSync('./build/index.html', { encoding: 'utf8' })
 
-server.use(express.static('build'))
+  server.use(express.static('build'))
+  console.log(`action=map-build-dir-to-routes`)
 
-server.get('/*', (req, res) => {
-  res.setHeader('content-type', 'text/html')
-  res.send(html)
-})
+  server.get('/*', (req, res) => {
+    res.setHeader('content-type', 'text/html')
+    res.send(html)
+  })
+  console.log(`action=setup-cactchall-route payload=app-html`)
 
-server.listen(PORT, ()=> {
-  console.log(`action=server-listen port=${ PORT }`)
-})
+  server.listen(PORT, ()=> {
+    console.log(`action=server-listen port=${ PORT }`)
+  })
+}
+
+start()
