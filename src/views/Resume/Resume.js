af70ab22b320bf855a04d56d6b25b0faee091fc7
Uzo Olisemeka <uzo@designbymobius.ca>
2022-07-22 19:26:08 -0400

    filter experience in resume view by experienc type

--- src/views/Resume/Resume.js
+++ src/views/Resume/Resume.js
@@ -1,13 +1,13 @@
-import { useState } from 'react'
+import { useEffect, useState } from 'react'
 import ViewTitle from '../../components/ViewTitle'
 import ViewSubtitle from '../../components/ViewSubtitle'
 import data from '../../data'
 import './style.css'
 
 const ExperienceNotesViewToggler = props => {
-  const { showNotes, onClick } = props
+  const { showNotes, toggleViewState } = props
   return (
-    <span onClick={ onClick }>
+    <span onClick={ toggleViewState }>
       {
         showNotes
           ? 'Hide Notes'
@@ -31,6 +31,8 @@ const Experience = props => {
   const expandedExperienceTechnologies = []
   const initialShowNotes = props.showNotes
   const [ showNotes, setShowNotes ] = useState(initialShowNotes ?? false)
+  const [ experienceFilters, setExperienceFilters ] = useState({})
+
 
   while (experienceTechnologies.length > 0) {
     const technologySlug = experienceTechnologies.shift()
@@ -99,7 +101,7 @@ const Experience = props => {
               <div className="Experience-notes-view-toggle">
                 <ExperienceNotesViewToggler
                   showNotes={ showNotes }
-                  onClick={ () => setShowNotes(!showNotes) }
+                  toggleViewState={ () => setShowNotes(!showNotes) }
                 />
               </div>
             )
@@ -111,17 +113,74 @@ const Experience = props => {
 
 const Resume = () => {
   const experiences = data?.experiences?.items ?? []
-  const sortedExperiences = [...experiences].sort((a,b) => {
-    // sort newer start date higher
-    if (a.start > b.start) return -1
-    if (a.start < b.start) return 1
+  const experienceTypes = data?.experiences?.types ?? []
 
-    // sort newer end date higher
-    if (a.end > b.end) return -1
-    if (a.end < b.end) return 1
+  // create filters data-store and updates api
+  const [ experienceFilters, updateExperienceFilters ] = useState({})
+  const typeFilter = {
+    add: type => {
+      let updated = false
+      let updates = { ...experienceFilters }
 
-    return 0
-  })
+      if (!updates.types) updates.types = []
+      if (!updates.types.includes(type)) {
+        updates.types.push(type)
+        updated = true
+      }
+
+      if (updated) updateExperienceFilters(updates)
+    },
+    del: type => {
+      let updated = false
+      let updates = { ...experienceFilters }
+
+      if (!updates.types) updates.types = []
+      if (updates.types.includes(type)) {
+        const typeIndex = updates.types.indexOf(type)
+        updates.types.splice(typeIndex, 1)
+        updated = true
+      }
+
+      if (updated) updateExperienceFilters(updates)
+    }
+  }
+
+  // filter experiences
+  const [ filteredExperiences, updateFilteredExperiences ] = useState([])
+  useEffect(() => {
+    if (!experienceFilters.types || experienceFilters.types.length === 0) {
+      updateFilteredExperiences(experiences)
+      return
+    }
+
+    const updatedFilteredExperiences = [...experiences].filter(experience => {
+      if (experienceFilters.types) {
+        if (!experienceFilters.types.includes(experience.type)) return false
+      }
+
+      return true
+    })
+
+    updateFilteredExperiences(updatedFilteredExperiences)
+  }, [ experiences, experienceTypes, experienceFilters ])
+
+  // sort filtered experiences
+  const [ sortedExperiences, setSortedExperiences ] = useState([])
+  useEffect(() => {
+    const updatedSortedExperiences = [...filteredExperiences].sort((a,b) => {
+      // sort newer start date higher
+      if (a.start > b.start) return -1
+      if (a.start < b.start) return 1
+
+      // sort newer end date higher
+      if (a.end > b.end) return -1
+      if (a.end < b.end) return 1
+
+      return 0
+    })
+
+    setSortedExperiences(updatedSortedExperiences)
+  }, [ filteredExperiences ])
 
   return (
     <>
@@ -129,12 +188,42 @@ const Resume = () => {
       <ViewSubtitle>
         Curated experience from <span className="emphasis">10+ years writing code</span>
       </ViewSubtitle>
+      <div className="Experiences-filters">
+        <div>Filter Experiences</div>
+        <div className="Experiences-filter Experiences-filter-type">
+          <div className="Experiences-filter-title">by type</div>
+          {
+            experienceTypes
+              .sort((a,b) => {
+                if (a.label > b.label) return 1
+                if (a.label < b.label) return -1
+                return 0
+              })
+              .map(experienceType => {
+                return (
+                  <div
+                    key={experienceType.slug}
+                    className={`Experiences-filter-type-option Experiences-filter-type-option-${experienceType.slug}${experienceFilters.types.includes(experienceType.slug) ? ' Experiences-filter-type-option-active' : ''}`}
+                    onClick={() => {
+                      if (!experienceFilters.types) return typeFilter.add(experienceType.slug)
+
+                      if (experienceFilters.types.includes(experienceType.slug)) typeFilter.del(experienceType.slug)
+                      else typeFilter.add(experienceType.slug)
+                    }}
+                  >
+                    { experienceType.label }
+                  </div>
+                )
+              })
+          }
+        </div>
+      </div>
       <div className="Experiences">
         {
-          experiences.length > 0
+          sortedExperiences.length > 0
             ? sortedExperiences.map(experience => {
                 const experienceType = data?.experiences?.types.find(type => type.slug === experience.type)
-                return <Experience data={experience} type={ experienceType } />
+                return <Experience data={experience} key={experience.slug} />
               })
             : <span>No Experience Found</span>
         }
