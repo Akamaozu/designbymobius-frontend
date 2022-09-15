7dba20049aa5efbe03bc8aef9f11a9849cb1e500
akamaozu <uzo@designbymobius.ca>
2022-09-15 07:51:20 -0400

    show description of filter settings and result tally

--- src/views/Resume/Resume.js
+++ src/views/Resume/Resume.js
@@ -5,15 +5,131 @@ import ExperienceFilters from './components/ExperienceFilters'
 import viewContext from './contexts/view'
 import './style.css'
 
-const { ViewProvider } = viewContext
+const { ViewProvider, useView } = viewContext
+
+const ExperienceTally = () => {
+  const [ state ] = useView()
+  const experiences = state?.experiences?.items ?? []
+  const experienceTypes = state?.experiences?.types ?? []
+  const isFiltered = state?.experiences?.isFiltered ?? false
+  const experienceFilters = state?.experiences?.filters ?? {}
+  const filteredExperiences = state?.experiences?.filteredExperiences
+  const technologiesMap = state?.technologies?.map ?? {}
+
+  const experienceTypesMap = experienceTypes.reduce(( typeMap, type ) => {
+    typeMap[ type.slug ] = type
+    return typeMap
+  }, {})
+
+  experienceFilters?.types?.sort()
+
+  return (
+    <div
+      style={{
+        margin: '2em',
+        fontWeight: 900,
+        lineHeight: '1.1em',
+        letterSpacing: '-1px',
+      }}
+    >
+      <div
+        style={{
+          fontSize: '125%',
+        }}
+      >
+        { experiences.length } Experiences
+      </div>
+      {
+        isFiltered && (
+          <div
+            style={{
+              fontSize: '125%',
+              color: 'limegreen',
+            }}
+          >
+            { filteredExperiences.length }
+            {
+              experienceFilters.types?.length > 0
+              ? experienceFilters.types?.length > 1
+                ? (
+                    ' '
+                    + experienceFilters.types.slice(0, -1).map(typeSlug => experienceTypesMap[ typeSlug ]?.label).join(', ')
+                    + ' or ' + experienceFilters.types.slice(-1).map(typeSlug => experienceTypesMap[ typeSlug ]?.label).join(', ')
+                  )
+                : ' '+ experienceFilters.types.map(typeSlug => experienceTypesMap[ typeSlug ].label)
+              : ''
+            }
+
+            {` Experience${filteredExperiences.length !== 1 ? 's':'' }`}
+            {
+              experienceFilters.technologies?.length > 0
+              ? experienceFilters.technologies?.length > 1
+                ? (
+                    ' using '
+                    + experienceFilters.technologies.slice(0, -1).map(slug => technologiesMap[ slug ]?.label).join(', ')
+                    + ' or ' + experienceFilters.technologies.slice(-1).map(slug => technologiesMap[ slug ]?.label).join(', ')
+                  )
+                : ' using '+ experienceFilters.technologies.map(slug => technologiesMap[ slug ].label)
+              : ''
+
+            }
+          </div>
+        )
+      }
+    </div>
+  )
+}
 
 const Resume = () => {
   return (
     <ViewProvider>
-      <ViewTitle>Resume</ViewTitle>
-      <ViewSubtitle>
-        Curated experience from <span className="emphasis">10+ years writing code</span>
-      </ViewSubtitle>
+      {
+        true && (
+          <>
+            <ViewTitle>Resume</ViewTitle>
+            <ViewSubtitle>
+              Curated experience from <span className="emphasis">10+ years writing code</span>
+            </ViewSubtitle>
+          </>
+        )
+      }
+      <div
+        style={{
+          marginLeft: '2em',
+        }}
+      >
+        <div
+          style={{
+            fontSize: '1.75em',
+            fontWeight: 900,
+            letterSpacing: '-1px',
+          }}
+        >
+          Uzo Olisemeka
+        </div>
+        <div
+          style={{
+
+          }}
+        >
+          👶 Born in Nigeria
+        </div>
+        <div
+          style={{
+
+          }}
+        >
+          🍁 Live in Canada
+        </div>
+        <div
+          style={{
+
+          }}
+        >
+          👨‍💻 Software Engineer with {(new Date()).getFullYear() - 2010 }+ years experience
+        </div>
+      </div>
+      <ExperienceTally />
       <ExperienceFilters />
       <ExperiencesList />
     </ViewProvider>
--- src/views/Resume/contexts/view.js
+++ src/views/Resume/contexts/view.js
@@ -1,6 +1,8 @@
-import { createContext, useContext, useMemo, useState } from 'react'
+import { createContext, useContext, useEffect, useMemo, useState } from 'react'
 import data from '../../../data'
+import utils from '../../../utils'
 
+const { getDependentTechnologies } = utils
 const ViewContext = createContext()
 const experiences = data?.experiences?.items ?? []
 const experienceTypes = data?.experiences?.types ?? []
@@ -21,9 +23,55 @@ const ViewProvider = props => {
   initialState = createStore(initialState, 'experiences', { items: experiences, types: experienceTypes, filters: {} })
   initialState = createStore(initialState, 'technologies', { items: technologies, types: technologyTypes })
 
+  initialState.technologies.map = initialState.technologies.items.reduce((technologyMap, technology) => {
+    technologyMap[technology.slug] = technology
+    return technologyMap
+  }, {})
+
   const [state, updateState] = useState(initialState)
-  const memoized = useMemo(() => [state, updateState], [state])
-  return <ViewContext.Provider value={memoized} {...props} />
+  // const memoized = useMemo(() => [state, updateState], [state])
+  const technologyMap = state.technologies.map
+  const experienceFilters = state.experiences.filters
+
+  // filter experiences
+  const [ isFiltered, updateIsFiltered ] = useState(false)
+  const [ filteredExperiences, updateFilteredExperiences ] = useState([])
+  useEffect(() => {
+    let filtered = false
+    const updatedFilteredExperiences = [...experiences].filter(experience => {
+
+      if (experienceFilters.types && experienceFilters.types.length > 0) {
+        filtered = true
+        if (experienceFilters.types.indexOf(experience.type) == -1) return false
+      }
+
+      if (experienceFilters.technologies && experienceFilters.technologies.length > 0) {
+        filtered = true
+
+        // check expanded technologies for filtered techs
+        const allExperienceTechnologies = getDependentTechnologies(experience.technologies, technologyMap)
+        let match = false
+        allExperienceTechnologies.forEach(technologySlug => {
+          if (match) return
+          if (experienceFilters.technologies.indexOf(technologySlug) > -1) match = true
+        })
+
+        return match
+      }
+
+      return true
+    })
+
+    const updatedState = { ...state }
+    updatedState.experiences.isFiltered = filtered ?? false
+    updatedState.experiences.filteredExperiences = updatedFilteredExperiences ?? []
+    updateState(updatedState)
+
+    updateIsFiltered(filtered)
+    updateFilteredExperiences(updatedFilteredExperiences)
+  }, [ state, isFiltered, filteredExperiences, experienceFilters, technologyMap ])
+
+  return <ViewContext.Provider value={[ state, updateState ]} {...props} />
 }
 
 const useView = () => {
