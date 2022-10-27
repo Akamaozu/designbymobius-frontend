fca3ceed2618c44423852aa999e30d3a31fea093
akamaozu <uzo@designbymobius.ca>
2022-10-27 19:42:26 -0400

    cache requested files in-memory to speed up server performance

--- server.js
+++ server.js
@@ -13,7 +13,11 @@ let server,
     cache = {},
     inflight = {},
     is_generating_pdfs,
-    generate_pdf_requests = []
+    generate_pdf_requests = [],
+    file_cache_paths = [
+      './build',
+      './public',
+    ]
 
 load_env_vars()
 load_data()
@@ -41,37 +45,125 @@ function start_server() {
   server = express()
   port = process.env.PORT ?? 3000
 
-  server.get(
-    [
-      '/favicon.ico',
-      '/logo192.png',
-      '/profile-pic.jpg',
-    ],
-    (req, res) => {
-      const filename = req.path.slice(1)
-      const cache_key = filename.replace('.', '-')
+  server.get('*', (req, res, next) => {
+    const path_md5 = md5(req.path)
+    const get_file_key = 'get-file-'+ path_md5
+    const file_exists_key = 'file-exists-'+ path_md5
 
-      if (cache[cache_key]) {
-        res.set('Content-Type', mime.lookup(filename))
-        return res.send(cache[cache_key])
+    const get_and_serve_file = async () => {
+      let file_path_on_disk
+
+      // file exists result not cached, get and cache
+      if (!cache.hasOwnProperty(file_exists_key)) {
+
+        // file exists check not inflight, start check
+        if (!inflight.hasOwnProperty(file_exists_key)) {
+          inflight[file_exists_key] = new Promise((resolve, reject) => {
+            let exists = false
+
+            const check_cache_paths_for_file = async () => {
+              for (i in file_cache_paths) {
+                const dir = file_cache_paths[i]
+                const exists_in_cache_path = await file_exists(dir + req.path)
+                if (exists_in_cache_path) {
+                  exists = true
+                  file_path_on_disk = dir + req.path
+                }
+              }
+
+              const file_exists_struct = { exists }
+              if (exists) file_exists_struct.path = file_path_on_disk
+
+              console.log(`action=check-disk-for-file req_path=${ req.path} exists=${ exists } file_path="${ file_path_on_disk }"`)
+
+              cache[file_exists_key] = file_exists_struct
+              delete inflight[file_exists_key]
+
+              resolve(file_exists_struct)
+            }
+
+            check_cache_paths_for_file()
+          })
+        }
       }
 
-      if (!inflight[cache_key]) inflight[cache_key] = get_file('./public' + req.path, { encoding: null })
+      await inflight[file_exists_key]
 
-      const get_and_send_file = async () => {
-        cache[cache_key] = await inflight[cache_key]
-        delete inflight[cache_key]
+      // file doesn't exist, let router handle request
+      if (!cache[file_exists_key].exists) return next()
 
-        res.set('Content-Type', mime.lookup(filename))
-        res.send(cache[cache_key])
+      // file content not cached, get and cache
+      if (!cache.hasOwnProperty(get_file_key)) {
+
+        // get file not inflight, start
+        if (!inflight.hasOwnProperty(get_file_key)) {
+          inflight[get_file_key] = new Promise((resolve, reject) => {
+            const get_file_from_disk = async () => {
+              console.log(`action=cache-file-from-disk-in-memory path="${ cache[file_exists_key].path }"`)
+
+              try {
+                const get_file_args = { encoding: null }
+                const filename = req.path.split('/').pop()
+                const file_ext = filename.split('.').pop()
+
+                switch (file_ext) {
+                  case 'html':
+                  case 'css':
+                  case 'js':
+                    get_file_args.encoding = 'utf-8'
+                  break
+                }
+
+                const content = await get_file(cache[file_exists_key].path, get_file_args)
+                cache[get_file_key] = {
+                  content,
+                  mimetype: mime.lookup(cache[file_exists_key].path),
+                }
+              }
+
+              catch(error) {
+                // no-op ... don't do anything drastic if get file fails
+                console.log(`action=cache-file-from-disk-in-memory-error path="${ cache[file_exists_key].path }" error="${ error.message}"`)
+              }
+
+              resolve(cache[get_file_key])
+              delete inflight[get_file_key]
+            }
+
+            get_file_from_disk()
+          })
+
+          await inflight[get_file_key]
+
+          ;(!cache.hasOwnProperty(get_file_key))
+            ? res.sendStatus(500)
+            : (() => {
+                res.set('content-type', cache[get_file_key].mimetype)
+                res.send(cache[get_file_key].content)
+              })()
+        }
+
+        // file exists check already running, wait for completion
+        else {
+          await inflight[get_file_key]
+
+          ;(!cache.hasOwnProperty(get_file_key))
+            ? res.sendStatus(500)
+            : (() => {
+                res.set('content-type', cache[get_file_key].mimetype)
+                res.send(cache[get_file_key].content)
+              })()
+        }
       }
 
-      get_and_send_file()
+      else {
+        res.set('content-type', cache[get_file_key].mimetype)
+        res.send(cache[get_file_key].content)
+      }
     }
-  )
 
-  server.use(express.static('build'))
-  console.log(`action=map-build-dir-to-server-routes`)
+    get_and_serve_file()
+  })
 
   // handle requests when app build hasn't started / isn't done
   server.use((req, res, next) => {
