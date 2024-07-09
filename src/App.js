91961de9a1db34d35dfde23be0bbeb8b7f84d8d9
uzo olisemeka <uzo@designbymobius.ca>
2024-07-09 14:40:25 -0400

    data: remove old dataset - technologies.json

--- src/App.js
+++ src/App.js
@@ -17,6 +17,7 @@ function App() {
             <Route path="/" element={ <Home /> } />
             <Route path="/resume" element={ <Resume /> } />
             <Route path="/projects" element={ <Resume /> } />
+            <Route path="/*" element={ <Home /> } />
           </Routes>
         </div>
         {
