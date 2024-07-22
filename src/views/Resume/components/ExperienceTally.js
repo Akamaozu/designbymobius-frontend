bb9877514346718f32078ac1ab54858ccfdc2710
uzo olisemeka <uzo@designbymobius.ca>
2024-07-22 16:42:43 -0400

    views: Resume/ExperienceTally: list tally of programming languages, databases and years of experience
    views: Resume/ExperienceTally: if resume is pdf and filtered, add link to unfiltered experiences

--- src/views/Resume/components/ExperienceTally.js
+++ src/views/Resume/components/ExperienceTally.js
@@ -15,6 +15,58 @@ const ExperienceTally = () => {
   const [ isFiltered, updateIsFiltered ] = useState()
   const [ filteredExperiences, updateFilteredExperiences ] = useState()
 
+  const experience_year_map = {}
+  experiences?.forEach( experience => {
+    const {
+      slug,
+      start,
+      end,
+    } = experience ?? {}
+
+    if (!slug || !start) return
+
+    if (!experience_year_map[ start ]) experience_year_map[ start ] = []
+
+    experience_year_map[ start ].push( slug )
+
+    if (start === end) return
+
+    const start_int = parseInt( start )
+    const end_int = end
+      ? parseInt( end )
+      : (new Date()).getFullYear()
+
+    if (start_int > end_int) return
+
+    for (var i = start_int + 1; i <= end_int; i += 1) {
+      if (!experience_year_map[ i ]) experience_year_map[ i ] = []
+
+      experience_year_map[ i ].push( slug )
+    }
+  })
+
+  const years_of_experience = Object
+    .keys( experience_year_map )
+    .length
+
+  const technology_type_map = state?.technologies?.items?.reduce(
+    ( map, technology ) => {
+      const {
+        slug,
+        type,
+      } = technology ?? {}
+
+      if (!map[ type ]) map[ type ] = []
+
+      map[ type ].push( slug )
+
+      return map
+    },
+    {}
+  )
+
+  console.log({ state, technology_type_map })
+
   useEffect(() => {
     if (!state?.experiences || !state?.technologies) return
 
@@ -54,37 +106,50 @@ const ExperienceTally = () => {
 
   return (
     <div className='Experiences-tally'>
-      <div className='Experiences-tally-unfiltered'>{ experiences.length } Experiences</div>
+      <div className='Experiences-tally-unfiltered'>
+        { years_of_experience } { years_of_experience === 1 ? 'Year' : 'Years' } of Experience.
+        {' '}
+        { technology_type_map[ 'language' ].length } { technology_type_map[ 'language' ].length === 1 ? 'Language' : 'Languages' }.
+        {' '}
+        { technology_type_map[ 'database' ].length } { technology_type_map[ 'database' ].length === 1 ? 'Database' : 'Databases' }.
+        {' '}
+        { experiences.length } { experiences.length === 1 ? 'Experience' : 'Experiences' }.
+      </div>
       {
         isFiltered && (
-          <div className='Experiences-tally-filtered'>
-            { filteredExperiences.length }
-            {
-              experienceFilters.types?.length > 0
-              ? experienceFilters.types?.length > 1
-                ? (
-                    ' '
-                    + experienceFilters.types.slice(0, -1).map(typeSlug => experienceTypesMap[ typeSlug ]?.label).join(', ')
-                    + ' or ' + experienceFilters.types.slice(-1).map(typeSlug => experienceTypesMap[ typeSlug ]?.label).join(', ')
-                  )
-                : ' '+ experienceFilters.types.map(typeSlug => experienceTypesMap[ typeSlug ].label)
-              : ''
-            }
+          <>
+            <div className='Experiences-tally-filtered'>
+              { filteredExperiences.length }
+              {
+                experienceFilters.types?.length > 0
+                ? experienceFilters.types?.length > 1
+                  ? (
+                      ' '
+                      + experienceFilters.types.slice(0, -1).map(typeSlug => experienceTypesMap[ typeSlug ]?.label).join(', ')
+                      + ' or ' + experienceFilters.types.slice(-1).map(typeSlug => experienceTypesMap[ typeSlug ]?.label).join(', ')
+                    )
+                  : ' '+ experienceFilters.types.map(typeSlug => experienceTypesMap[ typeSlug ].label)
+                : ''
+              }
 
-            {` Experience${filteredExperiences.length !== 1 ? 's':'' }`}
-            {
-              experienceFilters.technologies?.length > 0
-              ? experienceFilters.technologies?.length > 1
-                ? (
-                    ' using '
-                    + experienceFilters.technologies.slice(0, -1).map(slug => technologyMap[ slug ]?.label).join(', ')
-                    + ' or ' + experienceFilters.technologies.slice(-1).map(slug => technologyMap[ slug ]?.label).join(', ')
-                  )
-                : ' using '+ experienceFilters.technologies.map(slug => technologyMap[ slug ].label)
-              : ''
+              {` Experience${filteredExperiences.length !== 1 ? 's':'' }`}
+              {
+                experienceFilters.technologies?.length > 0
+                ? experienceFilters.technologies?.length > 1
+                  ? (
+                      ' using '
+                      + experienceFilters.technologies.slice(0, -1).map(slug => technologyMap[ slug ]?.label).join(', ')
+                      + ' or ' + experienceFilters.technologies.slice(-1).map(slug => technologyMap[ slug ]?.label).join(', ')
+                    )
+                  : ' using '+ experienceFilters.technologies.map(slug => technologyMap[ slug ].label)
+                : ''
 
-            }
-          </div>
+              }
+            </div>
+            <div className='Experiences-tally-filtered-caveat'>
+              <i>(view full list of experiences at: <a href={ `${ window.location.origin }/resume` } target="_blank" >{ window.location.origin }/resume</a>)</i>
+            </div>
+          </>
         )
       }
     </div>
