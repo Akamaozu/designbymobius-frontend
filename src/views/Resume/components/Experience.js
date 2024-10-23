a6acc5cb860729c32d5a83f87ebc6edb83ed85af
uzo olisemeka <uzo@designbymobius.ca>
2024-10-23 16:00:20 -0400

    views: Resume/Experience: group experience technologies, add labels to groupings

--- src/views/Resume/components/Experience.js
+++ src/views/Resume/components/Experience.js
@@ -20,6 +20,39 @@ const Experience = props => {
   const expandedExperienceTechnologies = getDependentTechnologies(experience.technologies, technologyMap)
   const [ showNotes, setShowNotes ] = useState(props?.showNotes ?? false)
 
+  const expanded_experience_technology_groupings = expandedExperienceTechnologies
+    .reduce(( map, technology_slug ) => {
+      const technology = technologyMap[ technology_slug ]
+
+      if (technology.type) {
+        if (!map.hasOwnProperty( technology.type )) map[ technology.type ] = []
+        map[ technology.type ].push( technology_slug )
+      }
+
+      if (technology.tags) {
+        technology.tags.forEach( technology_tag => {
+          if (!map.hasOwnProperty( technology_tag )) map[ technology_tag ] = []
+          map[ technology_tag ].push( technology_slug )
+        })
+      }
+
+      return map
+    }, {})
+
+  // sort group members alphabetically
+  Object
+    .keys( expanded_experience_technology_groupings )
+    .forEach( group_slug => {
+      expanded_experience_technology_groupings[ group_slug ].sort(( tech_a_slug, tech_b_slug ) => {
+        const tech_a = technologyMap[ tech_a_slug ]
+        const tech_b = technologyMap[ tech_b_slug ]
+
+        if (tech_a.label > tech_b.label) return 1
+        if (tech_a.label < tech_b.label) return -1
+        return 0
+      })
+    })
+
   let experienceTechnologiesClassName = 'Experience-technologies'
   if (experienceFilteredByTechnologies) experienceTechnologiesClassName += ' Experience-technologies-filtered'
 
@@ -31,49 +64,66 @@ const Experience = props => {
       </div>
       <div className="Experience-when">{ duration }</div>
       {
-        experience.technologies
-          ? (
-              <div className={experienceTechnologiesClassName}>
-                {
-                  expandedExperienceTechnologies
-                    .sort((a,b) => {
-                      const aTech = technologyMap[a].label
-                      const bTech = technologyMap[b].label
-
-                      if (aTech > bTech) return 1
-                      if (aTech < bTech) return -1
-
-                      return 0
-                    })
-                    .map(technologySlug => {
-                      const technology = technologyMap[technologySlug]
-                      const technologyDisplayName = technology.label
-                      const technologyTypeDisplayName = technologyTypeMap[technology.type]?.label
-                      const experienceTechnologiesFilterMatch = experienceFilters?.technologies?.indexOf?.(technology.slug) > -1
-
-                      let className = 'Experience-technology Experience-technology-'+ technology.slug
-                      if (experienceTechnologiesFilterMatch) className += ' Experience-technologies-filter-match'
-
-                      return (
-                        <div
-                          className={className}
-                          key={technology.slug}
-                        >
-                          { technologyDisplayName }
-                          <div className='Experience-technology-type'>
-                            { technologyTypeDisplayName }
-                          </div>
-                        </div>
-                      )
-                    })
-                }
-              </div>
-            )
+        experience.nutshell
+          ? <div className="Experience-nutshell">{ experience.nutshell }</div>
           : null
       }
       {
-        experience.nutshell
-          ? <div className="Experience-nutshell">{ experience.nutshell }</div>
+        experience.technologies
+          ? (
+              <div key={ experience.slug } className={experienceTechnologiesClassName}>
+                {
+                  expanded_experience_technology_groupings[ 'programming-language' ]
+                  && (
+                    <div key='programming-language' style={{ display: 'inline-block', marginRight: '1em' }}>
+                      <span style={{ textTransform: 'uppercase', fontSize: '.64em', color: '#888', fontWeight: 900 }}>Language:</span>
+                      {' '}
+                      { expanded_experience_technology_groupings[ 'programming-language' ].map( slug => <ExperienceTechnology key={ slug } slug={ slug } /> ) }
+                    </div>
+                  )
+                }
+                {
+                  expanded_experience_technology_groupings.backend
+                  && (
+                    <div key='backend' style={{ display: 'inline-block', marginRight: '1em' }}>
+                      <span style={{ textTransform: 'uppercase', fontSize: '.64em', color: '#888', fontWeight: 900 }}>Backend:</span>
+                      {' '}
+                      { expanded_experience_technology_groupings.backend.map( slug => <ExperienceTechnology key={ slug } slug={ slug } /> ) }
+                    </div>
+                  )
+                }
+                {
+                  expanded_experience_technology_groupings.frontend
+                  && (
+                    <div key='frontend' style={{ display: 'inline-block', marginRight: '1em' }}>
+                      <span style={{ textTransform: 'uppercase', fontSize: '.64em', color: '#888', fontWeight: 900 }}>Frontend:</span>
+                      {' '}
+                      { expanded_experience_technology_groupings.frontend.map( slug => <ExperienceTechnology key={ slug } slug={ slug } /> ) }
+                    </div>
+                  )
+                }
+                {
+                  expanded_experience_technology_groupings.database
+                  && (
+                    <div key='database' style={{ display: 'inline-block', marginRight: '1em' }}>
+                      <span style={{ textTransform: 'uppercase', fontSize: '.64em', color: '#888', fontWeight: 900 }}>Database:</span>
+                      {' '}
+                      { expanded_experience_technology_groupings.database.map( slug => <ExperienceTechnology key={ slug } slug={ slug } /> ) }
+                    </div>
+                  )
+                }
+                {
+                  expanded_experience_technology_groupings[ 'cloud-infrastructure' ]
+                  && (
+                    <div key='cloud-infrastructure' style={{ display: 'inline-block', marginRight: '1em' }}>
+                      <span style={{ textTransform: 'uppercase', fontSize: '.64em', color: '#888', fontWeight: 900 }}>Cloud:</span>
+                      {' '}
+                      { expanded_experience_technology_groupings[ 'cloud-infrastructure' ].map( slug => <ExperienceTechnology key={ slug } slug={ slug } /> ) }
+                    </div>
+                  )
+                }
+              </div>
+            )
           : null
       }
       {
@@ -99,6 +149,22 @@ const Experience = props => {
       }
     </div>
   )
+
+  function ExperienceTechnology({ slug }) {
+    const technology = technologyMap[ slug ]
+    if (!technology) return null
+
+    const is_filter_match = experienceFilters?.technologies?.indexOf?.(technology.slug) > -1
+
+    return (
+      <span className={ `Experience-technology ${ is_filter_match ? 'Experience-technologies-filter-match' : '' }` }>
+        { technology.label }
+        <div className='Experience-technology-type'>
+          { technologyTypeMap[ technology.type ].label }
+        </div>
+      </span>
+    )
+  }
 }
 
 export default Experience
