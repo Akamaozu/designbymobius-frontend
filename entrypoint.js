facc398eb481f8103e8e0ba0d00c9f45741bcad6
uzo olisemeka <uzo@designbymobius.ca>
2025-11-18 16:43:17 -0500

    entrypoint determines server start-up mode

--- entrypoint.js
+++ entrypoint.js
@@ -1,20 +1,39 @@
 const path = require('path')
 const supe = require('supe')
-const { exec } = require('node:child_process')
+const { spawn } = require('node:child_process')
 const utils = require('./utils')
 
 utils.env.load()
 
 const { ENV_NAME } = process.env
+const server_type = ENV_NAME?.toLowerCase().trim() === 'production'
+  ? 'production'
+  : 'development'
 
-if (ENV_NAME.toLowerCase() === 'production') {
-  console.log('action=start-server type=production')
+console.log(`action=start-server type=${server_type}`)
 
+if (server_type === 'production') {
   const supervisor = supe()
-  supervisor.start( 'server', path.join(__dirname, './server'))
+  supervisor.start( 'prod-server', path.join(__dirname, './server'))
 }
 
 else {
-  console.log('action=start-server type=development')
-  exec('npm run start:dev')
+  const server = spawn('npm', ['run', 'start:dev'], {
+    shell: true,
+  })
+
+  server.on('error', (error) => {
+    console.log(error)
+    throw error
+  })
+
+  server.stdout.on( 'data', (data) => {
+    const output = data.toString().trim()
+    console.log( `[dev-server] ${output}` )
+  })
+
+  server.stderr.on( 'data', (data) => {
+    const error = data.toString().trim()
+    console.log( `[dev-server][error] ${error}` )
+  })
 }
